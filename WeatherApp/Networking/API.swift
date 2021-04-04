3fd22c9519fd28ab1c7429b1156ea189ed8c7b8c
elizeurs <13ders@gmail.com>
2021-04-04 00:39:15 -0300

    NetworkManager

--- WeatherApp/Networking/API.swift
+++ WeatherApp/Networking/API.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 struct API {
-  static let key = ""
+  static let key = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=minutely&appid=7851ccae79e76935f159a49f6be64336"
 }
