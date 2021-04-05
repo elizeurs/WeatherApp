355e6ac3ac65fe58135c5a9e75183d2986d536c2
elizeurs <13ders@gmail.com>
2021-04-05 15:08:19 -0300

    City Name View

---
+++ WeatherApp/Views/CityNameView.swift
@@ -0,0 +1,30 @@
+//
+//  CityNameView.swift
+//  WeatherApp
+//
+//  Created by Elizeu RS on 05/04/21.
+//
+
+import SwiftUI
+
+struct CityNameView: View {
+  var city: String
+  var date: String
+  
+    var body: some View {
+      HStack {
+        VStack(alignment: .center, spacing: 10) {
+          Text(city)
+            .font(.title)
+            .bold()
+          Text(date)
+        }.foregroundColor(.blue)
+      }
+    }
+}
+
+struct CityNameView_Previews: PreviewProvider {
+    static var previews: some View {
+      CityNameView(city: "San Francisco", date: "mar 5, 2021")
+    }
+}
